c87519d5e967676997098aafda478905a8c3d2de
jaimejose <jaimejoseram.alumn@cescristorey.com>
2021-10-21 13:23:59 +0200

    hola

--- holamundo.cpp
+++ holamundo.cpp
@@ -2,4 +2,26 @@
 using namespace std;
 int main() {
 	cout << "Hola Mundo" << endl;
+	int numero1=0, numero2=0, result;
+	int num, num2, result1; 
+	double num1, num3, result2; 
+	cout << "introduce primer para la resta numero:  ";
+	cin >> numero1;
+	cout << "introduce segundo para la resta numero:  ";
+	cin >> numero2;
+	result = numero1 - numero2;
+	cout << "el resultado es   : " << result << endl; 
+	cout << "introduce numero para la multiplicacion: ";
+	cin >> num;
+	cout <<"introduce segundo numero: ";
+	cin >> num2;
+	result1 = num * num2;
+	cout <<"el resultado es :" <<result1 << endl;
+	cout <<"introduce numero para la division: ";	
+	cin >> num1;
+	cout <<"introduce segundo numero para la division: " ;
+	cin >> num3;	
+	result2 = num1 / num3;
+	cout << "el resultado es   : " << result2 << endl;
+	cout << endl;
 }
